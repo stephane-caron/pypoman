2eb5b0190d7db7ba589f4cfa6481d5e717ffe6cf
Stéphane Caron <stephane.caron@normalesup.org>
2021-02-06 14:16:25 +0100

    [doc] Move note to the correct function (^_^")

--- pypoman/duality.py
+++ pypoman/duality.py
@@ -85,14 +85,6 @@ def compute_polytope_halfspaces(vertices):
         Matrix of halfspace representation.
     b : array, shape=(m,)
         Vector of halfspace representation.
-
-    Notes
-    -----
-    This method won't work well if your halfspace representation includes
-    equality constraints :math:`A x == b` written as :math:`A x \\leq b \\wedge
-    -A x \\leq -b`. If this is your use case, consider using directly the
-    linear set ``lin_set`` of equality-constraint generators in `pycddlib
-    <https://pycddlib.readthedocs.io/en/latest/matrix.html>`_.
     """
     V = vstack(vertices)
     t = ones((V.shape[0], 1))  # first column is 1 for vertices
@@ -124,6 +116,14 @@ def compute_polytope_vertices(A, b):
     -------
     vertices : list of arrays
         List of polytope vertices.
+
+    Notes
+    -----
+    This method won't work well if your halfspace representation includes
+    equality constraints :math:`A x == b` written as :math:`A x \\leq b \\wedge
+    -A x \\leq -b`. If this is your use case, consider using directly the
+    linear set ``lin_set`` of equality-constraint generators in `pycddlib
+    <https://pycddlib.readthedocs.io/en/latest/matrix.html>`_.
     """
     b = b.reshape((b.shape[0], 1))
     mat = cdd.Matrix(hstack([b, -A]), number_type='float')
