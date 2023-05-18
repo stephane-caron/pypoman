5bb5aab55d9b17bf22a081e15a6a6e502ca25fb5
Stéphane Caron <stephane.caron@normalesup.org>
2023-05-18 15:17:55 +0200

    WIP: Unit test fixture for vertex enumeration

---
+++ tests/test_vertex_enumeration.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018-2020 Stephane Caron <stephane.caron@normalesup.org>
+#
+# This file is part of pypoman <https://github.com/stephane-caron/pypoman>.
+#
+# pypoman is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pypoman is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pypoman. If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests for vertex enumeration."""
+
+import unittest
+
+import numpy as np
+
+from pypoman import compute_polytope_vertices
+
+
+class TestVertexEnumeration(unittest.TestCase):
+    """Test fixture for vertex enumeration problems."""
+
+    def test_vertex_enumeration(self):
+
+        A = np.array(
+            [
+                [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+            ]
+        )
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 3])
+        vertices = compute_polytope_vertices(A, b)
+        self.assertGreater(len(vertices), 10)
