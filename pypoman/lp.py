a0a7e08076428dfb7cda5adee56da56ac2a09786
Stéphane Caron <stephane.caron@inria.fr>
2024-01-01 08:10:31 +0100

    Compress source headers by SPDX license identifiers

--- pypoman/lp.py
+++ pypoman/lp.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2018-2020 Stephane Caron <stephane.caron@normalesup.org>
-#
-# This file is part of pypoman <https://github.com/stephane-caron/pypoman>.
-#
-# pypoman is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# pypoman is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# pypoman. If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright 2018 Stéphane Caron
 
 """Functions for linear programming."""
 
